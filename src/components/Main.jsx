8edf25b9b616c0a77293ebcca37a9a88874fab87
VittorioDauria94 <dauriavittorio94@gmail.com>
2025-10-25 17:54:02 +0200

    feat: added jumbotron and some labels

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,33 @@
 import ShopBanner from "./ShopBanner";
+import comics from "../comics";
+import Jumbotron from "./Jumbotron";
 
 export default function Main() {
+  const comicsElem = comics.map((curComic) => (
+    <li key={curComic.id} className="col-16 flex justify-center">
+      <a href="#">
+        <img
+          className="comic-img"
+          src={curComic.thumb}
+          alt={`Cover image of ${curComic.title}`}
+        />
+        <h6>{curComic.title}</h6>
+      </a>
+    </li>
+  ));
   return (
     <main>
-      <div className="container py-20">
-        <h1>Content goes here</h1>
+      <Jumbotron />
+      <div className="container">
+        <span className="label current">CURRENT SERIES</span>
+      </div>
+      <ul className="container py-50 flex wrap justify-between">
+        {comicsElem}
+      </ul>
+      <div className="container mb-30 flex justify-center">
+        <a href="#" className="label load-more">
+          LOAD MORE
+        </a>
       </div>
       <ShopBanner />
     </main>
